55d75c2a88b6bde4bd84b83e2aa70ac581e8ce26
Matheus Almeida <matheus.almeida@csptecnologia.com>
2021-08-20 12:29:22 -0300

    initial settings

---
+++ src/pages/Teste/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ButtonPrimary from '../../components/Button/Primary'
+
+import { Container } from './styles'
+
+function Teste() {
+  return (
+    <Container>
+      <ButtonPrimary>Botão Primário</ButtonPrimary>
+    </Container>
+  )
+}
+
+export default Teste
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React from 'react'
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route } from 'react-router-dom'
 
-import CardSabin from "./pages/Login/CardSabin";
-import RegisterCustomerSabinHealthCard from "./pages/CustomerSabinHealthCard/Register";
-import Header from "./components/Header";
+import CardSabin from './pages/Login/CardSabin'
+import RegisterCustomerSabinHealthCard from './pages/CustomerSabinHealthCard/Register'
+import Header from './components/Header'
 import DefinePassword from './pages/Login/DefinePassword'
+import Teste from './pages/Teste'
 
-export default function Routes() {
+function Routes() {
   return (
     <Switch>
+      <Route exact path="/" component={Header} />
+
       <Route
         path="/cadastro-cliente-cartao-sabin-saude"
         component={RegisterCustomerSabinHealthCard}
       />
-      <Route exact path="/" component={Header} />
       <Route path="/cartao-sabin" component={CardSabin} />
       <Route path="/definir-senha" component={DefinePassword} />
+
+      <Route path="/teste" component={Teste} />
     </Switch>
-  );
+  )
 }
+
+export default Routes
