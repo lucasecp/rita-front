1df01d40c72813a2f74151824579bc2f8612f9fa
Matheus Almeida <matheus.almeida@csptecnologia.com>
2021-08-24 13:52:07 -0300

    mask cpf keyboard only numbers

--- src/components/Form/InputMask.js
+++ src/components/Form/InputMask.js
@@ -5,55 +5,30 @@ import mask from '../../helpers/mask'
 import { Container, Input } from './style'
 
 const InputMask = (props) => {
-  // const verifyTypeMask = (e) => {
-  //   if (props.typeMask === 'cpf') {
-  //     maskCpf(e)
-  //   }
-  //   if (props.typeMask === 'phone') {
-  //     maskPhone()
-  //   }
-  // }
-  // const maskCpf = (e) => {
-  //   let value = e.target.value
-  //   if (value.length === 3) value += '.'
-  //   if (value.length === 7) value += '.'
-  //   if (value.length === 11) value += '-'
-  //   props.setValue(value)
-  // }
-
-  // const maskPhone = () => {
-  //   let value = props.value
-  //   if (value.length === 0) value += '('
-  //   if (value.length === 3) value += ')'
-  //   if (value.length === 4) value += ' '
-  //   if (value.length === 10) value += '-'
-  //   props.setValue(value)
-  // }
-
   const containsNumbers = (value) => new RegExp('^[0-9]*$').test(value)
 
   const handleChange = (e) => {
     // Forçando o usuário a digitar somente números
     if (
-      !containsNumbers(e.target.value.replace(/(\.|\/|-)/g, '')) &&
-      props.mask === '###.###.###-##'
-    )
-      return
-
-    props.setValue(mask(e.target.value, props.mask))
+      !(
+        !containsNumbers(e.target.value.replace(/(\.|\/|-)/g, '')) &&
+        props.mask === '###.###.###-##'
+      )
+    ) {
+      props.setValue(mask(e.target.value, props.mask))
+    }
   }
 
   return (
     <Container>
       <label htmlFor={props.label}>{props.label}</label>
       <Input
-        type="text"
+        type="tel"
         id={props.label}
         onChange={handleChange}
         value={props.value}
         maxLength={props.maxLength}
         placeholder={props.placeHolder}
-        // onKeyDown={verifyTypeMask}
       />
     </Container>
   )
