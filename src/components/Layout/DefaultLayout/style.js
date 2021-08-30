b02cf14b11931362e443a703aa10b2df997fe44c
lucas <lucasecp7@gmail.com>
2021-08-30 13:51:53 -0300

    dashboard content

--- src/components/Layout/DefaultLayout/style.js
+++ src/components/Layout/DefaultLayout/style.js
@@ -3,9 +3,10 @@ import colors from '../../../styles/colors';
 export const Container = styled.div`
     display: grid;
     grid-template-columns: 300px 1fr;
-    grid-template-rows: 120px 1fr 70px;
+    grid-template-rows: auto 1fr auto;
     grid-template-areas: 'aside header' 'aside main' 'aside footer';
     min-height: 100vh;
+    background-color: #E5E5E5;
     >header{
       background: ${colors.feedback.success};
       grid-area: header;
@@ -17,6 +18,8 @@ export const Container = styled.div`
     >main{
       background: ${colors.text};
       grid-area: main;
+
+
     }
     >footer{
       background: ${colors.primary};
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -1,25 +1,54 @@
-import styled from 'styled-components';
-import colors from '../../../styles/colors';
-export const Container = styled.div`
-    display: grid;
-    grid-template-columns: 200px 1fr;
-    grid-template-rows: 120px 1fr 70px;
-    grid-template-areas: 'aside header' 'aside main' 'aside footer';
-    min-height: 100vh;
-    >header{
-      background: ${colors.feedback.success};
-      grid-area: header;
-    }
-    >aside{
-      background: ${colors.seconday};
-      grid-area: aside;
-    }
-    >main{
-      background: ${colors.text};
-      grid-area: main;
-    }
-    >footer{
-      background: ${colors.primary};
-      grid-area: footer;
-    }
-`;
+import styled, { css } from 'styled-components'
+import colors from '../../styles/colors'
+import { Container } from 'react-bootstrap'
+
+export const Box = styled(Container)`
+  background-color: #FFFFFF;
+  box-shadow: 0px 2px 8px 0px #DFD2FF26;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  border-radius: 8px;
+;
+`
+export const ButtonGroup = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+;
+`
+export const Card = styled.div`
+  padding: 24px;
+  border-radius: 8px;
+  h3 {
+    font-size: 24px;
+    line-height: 29px;
+    color: #F8F5FF
+  }
+  p {
+    max-width: 309px;
+    word-wrap: break-word;
+    font-size: 14px;
+    line-height: 17px;
+    font-weight: 400;
+    margin-top: 14px;
+    color: #F8F5FF;
+  }
+  button {
+    margin-top: 20px;
+  }
+  ${(props) =>
+    props.variation === 'light-blue' &&
+    css`
+      background: #c5dbfe;
+    `}
+  ${(props) =>
+    props.variation === 'dark-blue' &&
+    css`
+      background: #706BFF;
+    `}
+  ${(props) =>
+    props.variation === 'red' &&
+    css`
+      background: #DF644B;
+    `}
+`
