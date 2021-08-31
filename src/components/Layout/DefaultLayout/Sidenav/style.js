32bf366e0fb5680984711f763deb698054fca663
Matheus Almeida <matheus.almeida@csptecnologia.com>
2021-08-31 15:25:22 -0300

    Sidebar

--- src/components/Layout/DefaultLayout/Sidenav/style.js
+++ src/components/Layout/DefaultLayout/Sidenav/style.js
@@ -1,12 +1,47 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import colors from '../../../../styles/colors'
 
 export const Container = styled.aside`
   position: sticky;
-  /* left: 0;
-  top: 0; */
+  /* left: 0;*/
+  top: 0;
   height: 100vh;
-  width: 300px;
+  width: ${({ mode }) => (mode === 'expanded' ? 300 : 118)}px;
+  transition: 0.3s;
+
+  > div {
+    position: absolute;
+    top: 64px;
+    right: -20px;
+
+    padding: 14px 5px 14px 5px;
+
+    background: ${colors.background.sideBar};
+    border-radius: 0px 4px 4px 0px;
+
+    cursor: pointer;
+
+    > img {
+      transition: 0.3s;
+
+      ${({ mode }) =>
+        mode === 'short' &&
+        css`
+          transform: rotateY(180deg);
+        `}
+    }
+  }
+
+  ::before {
+    content: '';
+    width: 1px;
+    position: absolute;
+    top: 0;
+    right: -1px;
+
+    background: #efeafa;
+    height: inherit;
+  }
 
   > nav {
     position: relative;
@@ -15,31 +50,12 @@ export const Container = styled.aside`
     display: flex;
     flex-direction: column;
 
-    ::after {
-      content: '';
-      width: 1px;
-      position: absolute;
-      right: -1px;
-
-      background: #efeafa;
-      height: inherit;
-    }
-
-    > img {
-      position: absolute;
-      top: 48px;
-      right: -20px;
-
-      padding: 14px 5px 14px 5px;
-
-      background: ${colors.background.sideBar};
-      border-radius: 0px 4px 4px 0px;
-
-      cursor: pointer;
-    }
+    overflow: hidden;
 
     > header {
       > img {
+        min-width: ${({ mode }) => (mode === 'expanded' ? '212px' : 'unset')};
+
         padding: 40px 32px 52px;
       }
     }
--- src/components/Layout/DefaultLayout/style.js
+++ src/components/Layout/DefaultLayout/style.js
@@ -1,28 +1,26 @@
 import styled from 'styled-components'
 import colors from '../../../styles/colors'
 export const Container = styled.div`
-    display: grid;
-    grid-template-columns: 300px 1fr;
-    grid-template-rows: auto 1fr auto;
-    grid-template-areas: 'aside header' 'aside main' 'aside footer';
-    min-height: 100vh;
-    background-color: #E5E5E5;
-    >header{
-      background: ${colors.feedback.success};
-      grid-area: header;
-    }
-    >aside{
-      background: ${colors.background};
-      grid-area: aside;
-    }
-    >main{
-      background: ${colors.text};
-      grid-area: main;
-
-
-    }
-    >footer{
-      background: ${colors.primary};
-      grid-area: footer;
-    }
-`;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas: 'aside header' 'aside main' 'aside footer';
+  min-height: 100vh;
+  background-color: #e5e5e5;
+  > header {
+    background: ${colors.feedback.success};
+    grid-area: header;
+  }
+  > aside {
+    background: ${colors.background};
+    grid-area: aside;
+  }
+  > main {
+    background: ${colors.text};
+    grid-area: main;
+  }
+  > footer {
+    background: ${colors.primary};
+    grid-area: footer;
+  }
+`
