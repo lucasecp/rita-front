976d68dbb51ba0fc958f99e6af153f92a372ce9b
Evelyn de Paula <evelyn.paula@csptecnologia.com>
2021-09-08 16:04:40 -0300

    pré cadastro

--- src/pages/ConfDados/index.js
+++ src/pages/ConfDados/index.js
@@ -1,58 +1,74 @@
-import React from 'react'
-import { Col, Form } from 'react-bootstrap'
-import logoRitaVertical from '../../assets/logo/vertical-named-logo.svg'
-import OutlineButton from '../../components/Button/Outline'
-import ButtonPrimary from '../../components/Button/Primary'
-import InputText from '../../components/Form/InputText'
-import Footer from '../../components/Layout/DefaultLayout/Footer'
-import { Container, Content } from './styles'
+import OutlineButton from '@/components/Button/Outline'
+import ButtonPrimary from '@/components/Button/Primary'
+import InputMask from '@/components/Form/InputMask'
+import RegisterLayout from '@/components/Layout/RegisterLayout'
+import RadioButton from '@/styles/components/RadioButton'
+import { RadioGroup } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { Content } from './styles'
 
-function ConfDados() {
+function PreRegister() {
+  const [showModal] = useState(false)
+  const [message, setMessage] = useState(null)
+  const [choice, setChoice] = useState('')
+
+  const [phone, setPhone] = useState('')
+
+  const data = {
+    phone: '(**) *****-**23',
+  }
+
+  useEffect(() => {
+    if (data.phone) {
+      return setChoice('phone')
+    }
+  }, [])
+
+  function onChoiceChange(event) {
+    setChoice(event.target.value)
+  }
+
   return (
-    <Container>
-      <aside>
-        <img src={logoRitaVertical} />
-      </aside>
-      <main>
+    <>
+      <RegisterLayout>
         <Content>
-          <div xl={4} lg={6}>
-            <h6>
-              Para continuarmos, precisamos confirmar alguns dados. Reconhece
-              esse celular?
-            </h6>
-            <Form xl={4} lg={6}>
-              {['radio'].map((type) => (
-                <div key={`default-${type}`}>
-                  <Form.Check
-                    type={'radio'}
-                    id={`default-${type}`}
-                    label={`radio : ${type}`}
+          <h6>
+            Para continuarmos, precisamos confirmar alguns dados. <br />
+            Escolha uma das opções abaixo:
+          </h6>
+          <RadioGroup
+            aria-label="choice"
+            name="choice"
+            value={choice}
+            onChange={onChoiceChange}
+          >
+            {data.phone && (
+              <section>
+                <RadioButton
+                  value="phone"
+                  label={`Celular: ${data.phone}`}
+                  checked={choice === 'phone'}
+                />
+                {choice === 'phone' && (
+                  <InputMask
+                    mask="(##) #####-####"
+                    placeHolder="(00) 00000-0000"
+                    value={phone}
+                    setValue={setPhone}
+                    name="phone"
                   />
-                </div>
-              ))}
-            </Form>
-
-            <InputText mask="(##)#####-####" placeHolder="(00) 00000-0000" />
-            <Col
-              lg={6}
-              className="d-flex justify-content-lg-end justify-content-center align-items-center mt-4 mt-lg-0"
-            >
-              <ButtonPrimary xl={4} lg={6} className="mx-3">
-                Encaminhar
-              </ButtonPrimary>
-            </Col>
-            <Col
-              lg={6}
-              className="d-flex justify-content-lg-end justify-content-center align-items-center mt-2 mt-lg-0"
-            >
-              <OutlineButton>Não reconheço esses dados</OutlineButton>
-            </Col>
-          </div>
-          <Footer />
+                )}
+              </section>
+            )}
+          </RadioGroup>
+          <footer>
+            {choice && <ButtonPrimary>Encaminhar</ButtonPrimary>}
+            <OutlineButton>Não reconheço esses dados</OutlineButton>
+          </footer>
         </Content>
-      </main>
-    </Container>
+      </RegisterLayout>
+    </>
   )
 }
 
-export default ConfDados
+export default PreRegister
--- src/pages/ConfDados/styles.js
+++ src/pages/ConfDados/styles.js
@@ -1,86 +1,63 @@
+import colors from '@/styles/colors'
 import styled from 'styled-components'
-import colors from '../../styles/colors'
-import shape from '../../assets/img/element3.png'
 
-export const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  height: 100vh;
-  background-image: url(${shape});
-  background-repeat: no-repeat;
-  background-position: 0px -287px;
-  background-color: ${colors.background.primary};
-
-  > aside {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background: linear-gradient(
-      148.19deg,
-      #9146ff -15.68%,
-      #8f46fe -3.23%,
-      rgba(136, 70, 252, 0.992703) 43.97%,
-      #1c23bd 119.58%
-    );
-    opacity: 0.8;
-
-    > img {
-      margin-top: 104px;
-      width: 96px;
-    }
-  }
-
-  > main {
-    padding: 104px 88px 24px;
-  }
-
-  @media (max-width: 630px) {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    flex-wrap: wrap;
-  }
-`
-
-export const Content = styled.section`
+export const Content = styled.div`
+  background: #fff;
+  box-shadow: 0px 2px 8px rgba(223, 210, 255, 0.15);
+  border-radius: 8px;
+  padding: 38px 32px 24px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
+
+  > h6 {
+    font-size: 24px;
+    font-weight: 500;
+    color: ${colors.gray.dark};
+    margin-bottom: 32px;
+  }
 
   > div {
-    background: rgba(255, 255, 255, 0.8);
-    box-shadow: 0px 2px 8px rgba(223, 210, 255, 0.15);
-    border-radius: 8px;
-    padding: 40px 32px 48px;
+    > section {
+      > div + div {
+        margin-top: 16px;
+      }
 
-    > h6 {
-      font-weight: 700;
-      font-size: 24px;
-      line-height: 140%;
-      color: ${colors.text.link};
-    }
-
-    > Form {
-      padding: 22px 0;
-      font-family: Athletics;
-      font-style: normal;
-      font-weight: 700;
-      font-size: 16px;
-      color: ${colors.background.activeLink};
-      cursor: pointer;
-    }
-
-    > div {
-      display: flex;
-      padding: 15px 0;
-      justify-content: flex-end;
-      margin-right: 24px;
+      + section {
+        margin-top: 24px;
+      }
     }
   }
 
-  @media (max-width: 630px) {
-    display: flex;
-    flex-direction: column;
+  > footer {
+    align-self: flex-end;
+    margin-top: 24px;
+
+    > button + button {
+      margin-left: 24px;
+    }
+  }
+
+  @media (max-width: 900px) {
+    > footer {
+      align-self: center;
+    }
+  }
+
+  @media (max-width: 800px) {
+    padding: 32px 16px 24px;
+  }
+
+  @media (max-width: 550px) {
+    > footer {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+
+      > button + button {
+        margin-left: 0px;
+
+        margin-top: 16px;
+      }
+    }
   }
 `
