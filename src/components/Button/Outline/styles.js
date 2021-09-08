4ff5c7b81db2ce58b80ab09b61efca5abd1c4687
Evelyn de Paula <evelyn.paula@csptecnologia.com>
2021-09-08 10:37:47 -0300

    merge

--- src/components/Button/Outline/styles.js
+++ src/components/Button/Outline/styles.js
@@ -1,9 +1,5 @@
 import styled, { css } from 'styled-components'
-<<<<<<< HEAD
-import colors from '../../../styles/colors'
-=======
 import colors from '@/styles/colors'
->>>>>>> fc4cffd9aec41606e8e85507c7d951477b03449d
 
 export const Container = styled.button`
   border: 2px solid ${colors.secondary};
@@ -24,31 +20,18 @@ export const Container = styled.button`
       padding: 10px 16px !important;
       :hover {
         background: #fff !important;
-<<<<<<< HEAD
-        color: ${colors.text.link} !important;
-=======
         color: ${colors.gray.dark} !important;
->>>>>>> fc4cffd9aec41606e8e85507c7d951477b03449d
       }
     `}
   ${(props) =>
     props.variation === 'green' &&
     css`
-<<<<<<< HEAD
-      color: ${colors.feedback.activeLinkSpan};
-      padding: 10px 16px !important;
-      border: solid 2px ${colors.feedback.activeLinkSpan};
-      :hover {
-        color: ${colors.green.light} !important;
-        background: ${colors.feedback.activeLinkSpan} !important;
-=======
       color: ${colors.green.light};
       padding: 10px 16px !important;
       border: solid 2px ${colors.green.light};
       :hover {
         color: ${colors.green.dark} !important;
         background: ${colors.green.light} !important;
->>>>>>> fc4cffd9aec41606e8e85507c7d951477b03449d
       }
     `}
     ${(props) =>
@@ -62,15 +45,9 @@ export const Container = styled.button`
       }
     `}
   :hover {
-<<<<<<< HEAD
-    background-color: ${colors.secondary};
-    transition: 0.3s;
-    color: #fff;
-=======
     background-color: #7338cb;
     transition: 0.3s;
     color: #fff;
     border-color: #7338cb;
->>>>>>> fc4cffd9aec41606e8e85507c7d951477b03449d
   }
 `
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-<<<<<<< HEAD
-import Header from './components/HeaderTest'
-import DefinePassword from './pages/Login/DefinePassword'
-import Teste from './pages/Teste'
-import Initial from './pages/RegisterOld/CardSabin/Initial'
-import ConfirmData from './pages/RegisterOld/CardSabin/ConfirmData'
-import Dashboard from './pages/Dashboard'
-import Profile from './pages/Profile'
-import ConfDados from './pages/ConfDados'
-=======
 import Header from '@/components/HeaderTest'
 import Teste from '@/pages/Teste'
 import Profile from '@/pages/Profile'
->>>>>>> fc4cffd9aec41606e8e85507c7d951477b03449d
 
 import RegisterCardSabin from '@/pages/Register/InitialRegister/CardSabin'
 import InitialRegister from '@/pages/Register/InitialRegister/Default'
@@ -25,17 +14,9 @@ function Routes() {
     <Switch>
       <Route exact path="/" component={Header} />
 
-<<<<<<< HEAD
-      {/* Rota Evelyn */}
-      <Route path="/confDados" component={ConfDados} />
-
-      <Route path="/cadastro" component={Register} />
-      {/* <Route path="/cadastro" component={Register} /> */}
-=======
       <Route path="/cadastro-pre" component={PreRegister} />
       <Route path="/cadastro-inicial" component={InitialRegister} />
       <Route path="/cadastro-cartao-sabin" component={RegisterCardSabin} />
->>>>>>> fc4cffd9aec41606e8e85507c7d951477b03449d
 
       <Route path="/master-page" component={Profile} />
 
