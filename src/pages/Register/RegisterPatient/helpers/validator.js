53317dee4ae585d25dd63aca67a89a54e9f92e20
lucas <lucasecp7@gmail.com>
2021-09-14 16:06:00 -0300

    validator

--- src/pages/Register/RegisterPatient/helpers/validator.js
+++ src/pages/Register/RegisterPatient/helpers/validator.js
@@ -1,32 +1,32 @@
-export const validateName = (value) => {
-  if (!value.trim()) return {name: 'Campo Obrigatório'};
+// export const validateName = (value) => {
+//   if (!value.trim()) return {name: 'Campo Obrigatório'};
 
-}
- export const validateEmail = () =>{
-   if (!values.email) {
-      errors.email = 'Email Obrigatório';
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-      errors.email = 'Email inválido.';
-    }
-  }
- }
-  if (values.confirmEmail !== values.email) {
-    errors.confirmEmail = 'Email';
-  }
-  if (!values.cpf) {
-    errors.cpf = 'Campo cpf Obrigatório';
-  }
-  if (!values.phone) {
-    errors.phone = 'Campo telefone Obrigatório';
-  }
-  if (!values.gender) {
-    errors.gender = 'Campo gênero Obrigatório';
-  }
-  if (!values.terms) {
-    errors.terms = 'Campo termo Obrigatório';
-  }
-  if (!values.birthdate) {
-    errors.birthdate = 'Campo Nascimento Obrigatório';
-  }
-  return errors;
-}
+// }
+//  export const validateEmail = (value) =>{
+//    if (!value) {
+//      { email: 'Email Obrigatório'};
+//     } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+//      { email :'Email inválido.'}
+//     }
+//  }
+//  export const validateCpf = (value) =>{
+//     if (!value) {
+//     {cpf : 'Campo cpf Obrigatório';}
+//   }
+// }
+// // export const validateEmail = (value) =>{
+//    if (!value) {
+//     errors.phone = 'Campo telefone Obrigatório';
+//   }
+// }
+//   if (!values.gender) {
+//     errors.gender = 'Campo gênero Obrigatório';
+//   }
+//   if (!values.terms) {
+//     errors.terms = 'Campo termo Obrigatório';
+//   }
+//   if (!values.birthdate) {
+//     errors.birthdate = 'Campo Nascimento Obrigatório';
+//   }
+//   return errors;
+// }
