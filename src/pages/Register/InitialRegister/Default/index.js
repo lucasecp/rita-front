ebbc18fd7e786e9111b84ba028f4388abd3d837c
lucas <lucasecp7@gmail.com>
2021-09-17 16:28:52 -0300

    routes with cpf

--- src/pages/Register/InitialRegister/Default/index.js
+++ src/pages/Register/InitialRegister/Default/index.js
@@ -70,7 +70,7 @@ function DefaultRegister() {
     } catch ({ response }) {
       const apiStatus = response.status
       if (apiStatus === status.NOT_COSTUMER_CARD_SABIN) {
-        return history.push('/cadastro/paciente')
+        return history.push('/cadastro/paciente',{cpf})
       }
     } finally {
       setLoading(false)
