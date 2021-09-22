fe963fe52b61d970a5711f274d0eae24dd11f6ed
Matheus Almeida <matheus.almeida@csptecnologia.com>
2021-09-22 16:53:15 -0300

    PDF Preview on Mobile

--- src/helpers/downloadFile.js
+++ src/helpers/downloadFile.js
@@ -2,10 +2,10 @@ const downloadFile = (file) => {
   const link = document.createElement('a')
   link.href = window.URL.createObjectURL(file)
   link.target = '_blank'
-  // link.download = `${+new Date()}.pdf`
+  link.download = `${+new Date()}.pdf`
 
   link.click()
-  // link.remove()
+  link.remove()
 }
 
 export default downloadFile
