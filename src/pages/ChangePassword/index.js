5869afef2245da070ef851dce92a207e3e97036f
Matheus Almeida <matheus.almeida@csptecnologia.com>
2021-10-27 12:06:13 -0300

    Remove Warnings 2

--- src/pages/ChangePassword/index.js
+++ src/pages/ChangePassword/index.js
@@ -1,7 +1,7 @@
 import DefaultLayout from '../../components/Layout/DefaultLayout'
 import React from 'react'
 
-// import { Container } from './styles'
+import { Container } from './styles'
 
 function ChangePassword() {
   return (
