6e4fcc00a3d8e1ba9d7dc2595528d041e65c69be
lucas <lucasecp7@gmail.com>
2021-11-12 15:02:46 -0300

    filename pdf2

--- src/helpers/downloadFile.js
+++ src/helpers/downloadFile.js
@@ -1,8 +1,20 @@
-const downloadFile = (file, filename) => {
+const downloadFile = (file) => {
+  const date = new Date()
+  const dateName =
+    date.getFullYear() +
+    ('00' + (date.getMonth() + 1)).slice(-2) +
+    ('00' + date.getDate()).slice(-2) +
+    '_' +
+    ('00' + (date.getHours())).slice(-2) +
+    ('00' + date.getMinutes()).slice(-2) +
+    ('00' + date.getSeconds()).slice(-2) +
+    '_' +
+    'Autorizacoes'
+
   const link = document.createElement('a')
   link.href = window.URL.createObjectURL(file)
   link.target = '_blank'
-  link.download = `${+new Date() + filename || ''}.pdf`
+  link.download = `${dateName}.pdf`
 
   link.click()
   link.remove()
