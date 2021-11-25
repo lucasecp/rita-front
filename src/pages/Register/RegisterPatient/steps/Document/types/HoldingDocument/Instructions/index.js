c09e43dcf04fc3d26e098b13ecaf85ce715341c3
Andre Lessa <andre.lessa@csptecnologia.com>
2021-11-25 16:31:41 -0300

    SRI 568
    SRI 568 V1

--- src/pages/Register/RegisterPatient/steps/Document/types/HoldingDocument/Instructions/index.js
+++ src/pages/Register/RegisterPatient/steps/Document/types/HoldingDocument/Instructions/index.js
@@ -35,13 +35,11 @@ function InstructionsHoldingDocuments({ onGetFile }) {
             O envio da sua foto segurando o documento de identificação é
             obrigatório.
           </p>
-          <span>
-            Tamanho máximo do arquivo: 10MB
-            <br />
-            Tipos de arquivos aceitos: jpg, jpeg, png ou pdf.
-            <br />
-            Permitido apenas o upload de 1 arquivo.
-          </span>
+          <div>
+        <span>Permitido apenas o upload de 1 arquivo</span>
+        <span>Tamanho máximo do arquivo: 10MB</span>
+        <span>Tipos de arquivos aceitos: jpg, jpeg, png ou pdf.</span>
+        </div>
         </aside>
       </div>
     </Container>
