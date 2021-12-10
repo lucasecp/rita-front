2ad0f22c061e8e155c92a92d3c49f8cdb5ce5d83
lucas <lucasecp7@gmail.com>
2021-12-10 15:25:26 -0300

    arrow left icon

---
+++ src/pages/modules/patient/ClinicInformation/index.js
@@ -0,0 +1,19 @@
+import { DefaultLayout } from '@/components/Layout/DefaultLayout'
+import React from 'react'
+import { Content } from './styles'
+import { Link } from 'react-router-dom'
+import { PATIENT_SCHEDULE_APPOINTMENT } from '@/routes/constants/namedRoutes/routes'
+import {ReactComponent as ArrowLeftIcon} from '@/assets/icons/arrow-left2.svg'
+
+const ClinicInformation = () => {
+  return (
+    <DefaultLayout title="Informações da Clínica">
+      <Content>
+        <div>
+          <Link to={PATIENT_SCHEDULE_APPOINTMENT}><ArrowLeftIcon/> Voltar aos resultados</Link>
+        </div>
+      </Content>
+    </DefaultLayout>
+  )
+}
+export default ClinicInformation
--- src/routes/modules/patient/patient.routes.js
+++ src/routes/modules/patient/patient.routes.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Route from '../../custom.routes'
 import {
+  PATIENT_CLINIC_INFORMATION,
   PATIENT_SCHEDULE_APPOINTMENT
 } from '../../constants/namedRoutes/routes'
 import ScheduleAppointment from '@/pages/modules/patient/ScheduleAppointment'
+import ClinicInformation from '@/pages/modules/patient/ClinicInformation'
 
 const patientRoutes = [
   {
     path: PATIENT_SCHEDULE_APPOINTMENT,
     component: ScheduleAppointment,
+  },
+  {
+    path: PATIENT_CLINIC_INFORMATION,
+    component: ClinicInformation, 
   },
 ]
 
