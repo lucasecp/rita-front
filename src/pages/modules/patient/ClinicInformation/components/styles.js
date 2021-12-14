60343a3ae866c0ba2d0bf42f366b9fa74c3f2f23
lucas <lucasecp7@gmail.com>
2021-12-14 15:34:33 -0300

    595 v2

--- src/pages/modules/patient/ClinicInformation/components/styles.js
+++ src/pages/modules/patient/ClinicInformation/components/styles.js
@@ -1,5 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components'
 
 export const Accordion = styled.div`
-  
-`;
+  max-height: 0;
+  transition: 0.4s;
+  visibility: hidden;
+  transition-delay: .1s;
+  /* ${({ order }) => {
+    order === 'show' &&
+    css`
+        max-height: 1000px;
+        visibility: visible;
+        `
+  }} */
+  &[data-expanded='show'] {
+    transition: 0.4s max-height;
+    max-height: 2000px;
+    visibility: visible;
+  }
+`
