733b7905347ff2f5d8f5ab8df82a8d7fc27bbcfb
lucas <lucasecp7@gmail.com>
2021-12-15 15:09:17 -0300

    595 v4

--- src/pages/modules/patient/DoctorInformation/index.js
+++ src/pages/modules/patient/DoctorInformation/index.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom'
 import { Content } from './styles'
 import { ReactComponent as ArrowLeftIcon } from '@/assets/icons/arrow-left2.svg'
 import ClinicItem from './components/ClinicItem'
-import ClinicItemDetails from './components/ClinicItemDetails'
 
 const DoctorInformation = () => {
   useEffect(() => {
     document.title = 'Rita Saúde | Informações do Médico'
   }, [])
+
   return (
     <DefaultLayout title="Informações do Especialista">
       <Content>
@@ -23,7 +23,6 @@ const DoctorInformation = () => {
         <Header />
         <h3>Clinicas que atende</h3>
         <ClinicItem />
-        <ClinicItemDetails />
       </Content>
     </DefaultLayout>
   )
