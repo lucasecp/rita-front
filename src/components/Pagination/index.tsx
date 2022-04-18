1c3d720e83afa46fbb5dee64af0e080ae6cc7a4a
Felipe Miguel dos Santos <felipe.miguel@csptecnologia.com>
2022-04-18 09:39:39 -0300

    SRI 1198

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -21,7 +21,8 @@ const Pagination: React.FC<PaginationProps> = ({
   )
   const [currentPage, setCurrentPage] = useState(Number(query.get('page')) || 1)
 
-  const skipedPages = (currentPage - 1) * Number(limit)
+  //const skipedPages = (currentPage - 1) * Number(limit)
+  const [skipedPages, setSkipedPages] = useState(0)
 
   const totalPages = Math.ceil(total / Number(limit)) || 0
 
@@ -29,13 +30,24 @@ const Pagination: React.FC<PaginationProps> = ({
 
   const queryApiString = `?limit=${limit}&skip=${skipedPages}`
 
-  const currentTotal =
-    currentPage === totalPages ? total : skipedPages + Number(limit)
+  //const currentTotal = currentPage === totalPages ? total : skipedPages + Number(limit)
+  const [currentTotal, setCurrentTotal] = useState(0)
 
   useEffect(() => {
     history.push(queryString)
     setQuery(queryApiString)
-  }, [limit, currentPage, restQuery])
+    setCurrentTotal(currentPage === totalPages ? total : skipedPages + Number(limit))
+    setSkipedPages((currentPage - 1) * Number(limit))
+  }, [limit, currentPage])
+
+  useEffect(() => {
+    const queryString = `?page=${1}&limit=${limit}${restQuery || ''}`
+    setSkipedPages((1 - 1) * Number(limit))
+    setCurrentTotal(1 === totalPages ? total : skipedPages + Number(limit))
+    console.log({currentTotal, skipedPages})
+    history.push(queryString)
+    setQuery(queryApiString)
+  }, [restQuery])
 
   useEffect(() => {
     if (currentPage > totalPages && total > 0) {
@@ -46,11 +58,15 @@ const Pagination: React.FC<PaginationProps> = ({
   const prevPage = () => {
     if (currentPage === 1) return
     setCurrentPage(currentPage - 1)
+    setCurrentTotal(currentPage === totalPages ? total : skipedPages + Number(limit))
+    setSkipedPages((currentPage - 1) * Number(limit))
   }
 
   const nextPage = () => {
     if (currentPage === totalPages || !totalPages) return
     setCurrentPage(currentPage + 1)
+    setCurrentTotal(currentPage === totalPages ? total : skipedPages + Number(limit))
+    setSkipedPages((currentPage - 1) * Number(limit))
   }
 
   return (
