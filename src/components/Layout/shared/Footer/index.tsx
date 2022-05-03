0789cfd43993faeb3726489bcce2110e5b051b93
bitbucket-pipelines <commits-noreply@bitbucket.org>
2022-05-03 18:13:11 +0000

    Versao atualizada [skip ci]

--- src/components/Layout/shared/Footer/index.tsx
+++ src/components/Layout/shared/Footer/index.tsx
@@ -25,7 +25,7 @@ export const Footer: React.FC = () => {
   return (
     <Container>
       <LogoIcon />
-      <CustomTooltip label="v0.19.2.971">
+      <CustomTooltip label="v0.19.3.972">
         <h6>Rita Saúde</h6>
       </CustomTooltip>
       <Time className="text-end">{dateNow}</Time>
