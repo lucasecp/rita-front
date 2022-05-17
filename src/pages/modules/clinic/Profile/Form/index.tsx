9a69efaaceb92a67a51406a4efb338ac2e9b66d3
lucas <lucasecp7@gmail.com>
2022-05-17 17:33:04 -0300

    adjust

--- src/pages/modules/clinic/Profile/Form/index.tsx
+++ src/pages/modules/clinic/Profile/Form/index.tsx
@@ -95,20 +95,7 @@ const FormClinicProfile: React.FC = () => {
   }, [isDisabled])
 
   const save = async (photo: File, data: any) => {
-    const formData = new FormData()
-    const keys = Object.keys(data)
-    keys.forEach((key) => {
-      formData.append(
-        key,
-        key === 'especialidade' ? JSON.stringify(data[key]) : data[key],
-      )
-    })
-
-    if (photo.size !== 0) {
-      formData.append('file', photo)
-    }
-
-    await apiAdmin.put(`/clinica/minha-clinica/${user.idClinica}`, formData)
+    await apiAdmin.put(`/clinica/minha-clinica/${user.idClinica}`, data)
   }
 
   /** @description Envia os dados modificados para o backend */
