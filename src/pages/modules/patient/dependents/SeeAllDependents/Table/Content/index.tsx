33e63d80d993fa83b0cf449614ff14fed9379a04
Arthur Gamm <arthur.gramm@csptecnologia.com>
2022-05-18 09:28:16 -0300

    feature/ajust code in grid dependents/SRI-1460

--- src/pages/modules/patient/dependents/SeeAllDependents/Table/Content/index.tsx
+++ src/pages/modules/patient/dependents/SeeAllDependents/Table/Content/index.tsx
@@ -6,24 +6,30 @@ import { ContentProps } from '../../types'
 const Content: React.FC<ContentProps> = ({ dependents }) => {
   return (
     <Container>
-      {dependents?.map((dep, index) => (
-        <ListItem key={index} warning={!dep.documentsOk || dep.isValidate}>
-          <li>{dep.name}</li>
-          <li>{dep.birthdate}</li>
-          <li>{dep.cpf}</li>
-          <li>{dep.status}</li>
-          <Actions
-            idDependent={dep.id}
-            status={dep.status}
-            warning={
-              !dep.documentsOk || dep.isValidate || dep.status === 'Negado'
-            }
-            documentsOk={dep.documentsOk}
-            isValidate={dep.isValidate}
-            dependent={{ id: dep.id, birthdate: dep.birthdate, cpf: dep.cpf }}
-          />
-        </ListItem>
-      ))}
+      {dependents?.map((dep, index) => {
+        // Não tiver documento
+        // Validação expirou
+        // Status Negado
+        const dependentHasPendency =
+          !dep.documentsOk || dep.isValidate || dep.status === 'Negado'
+
+        return (
+          <ListItem key={index} warning={dependentHasPendency}>
+            <li>{dep.name}</li>
+            <li>{dep.birthdate}</li>
+            <li>{dep.cpf}</li>
+            <li>{dep.status}</li>
+            <Actions
+              idDependent={dep.id}
+              status={dep.status}
+              warning={dependentHasPendency}
+              documentsOk={dep.documentsOk}
+              isValidate={dep.isValidate}
+              dependent={{ id: dep.id, birthdate: dep.birthdate, cpf: dep.cpf }}
+            />
+          </ListItem>
+        )
+      })}
       {!dependents.length && <h2>Nenhum dependente cadastrado</h2>}
     </Container>
   )
